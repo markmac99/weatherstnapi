af746f055bcd49f49362a5e09ecc7982548f6c10
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-07-07 01:04:30 +0100

    move to new server

--- getBresserData.sh
+++ getBresserData.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $here
-source /home/ec2-user/venvs/openhabstuff/bin/activate
+source /home/bitnami/venvs/openhabstuff/bin/activate
 python getwu.py
---
+++ installGetWu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# copyright Mark McIntyre, 2023-
+
+# install WU service to read from my bresser
+
+here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $here
+
+sudo cp getwu.service /etc/systemd/system/
+sudo systemctl enable getwu
+sudo systemctl start getwu
