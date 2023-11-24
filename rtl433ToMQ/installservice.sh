6458ba5478c983c101a7ae67201db644a43d4559
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-11-24 14:09:31 +0000

    document other requirements

--- rtl433ToMQ/installservice.sh
+++ rtl433ToMQ/installservice.sh
@@ -2,7 +2,13 @@
 
 # copyright Mark McIntyre, 2023-
 
-# install WU service to read from my bresser
+# install service to read from my WH1080 / Maplin weatherstation outdoor sensors
+
+# pretty sure this needs apt-get install rtl-sdr librtlsdr-dev
+# rtl_433 should be downloaded from github and built locally
+
+# rtl_433 should be able to publish to MQ but I'm having problems with it. 
+# might work on a new build pi
 
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $here
