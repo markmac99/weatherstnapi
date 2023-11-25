eeef79e8210b474544d65e2161bb0bcc3c6bbd02
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-11-25 11:08:45 +0000

    reload daemon after installing service

--- getwu/installGetWu.sh
+++ getwu/installGetWu.sh
@@ -8,5 +8,6 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $here
 
 sudo cp getwu.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable getwu
 sudo systemctl start getwu
